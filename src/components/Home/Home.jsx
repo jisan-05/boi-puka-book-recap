c103680ddb7101f5b880e39944cbdf31183c7d69
Tanvir <zishantanvir05@gmail.com>
2024-12-27 10:27:10 +0600

    navbar added

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-2xl mx-auto">This is Home Page</h2>            
+        </div>
+    );
+};
+
+export default Home;
